a9eb5b52ec8a58afdd30ec4eae31d80c2d1ef8d5
Narul <naruln9494@gmail.com>
2021-05-23 08:42:29 +0600

    Shop

--- src/App.vue
+++ src/App.vue
@@ -1,99 +1,27 @@
 <template>
-
-    <section class="score">
-      Player <span>{{ this.win_count }}</span> x 
-     <span>{{ this.lose_count }}</span> Computer
-    </section>
-
-    <template v-if="this.question">
-      <h1 v-html="this.question">
-      </h1>
-
-      <template v-for="(answer, index) in this.answers" v-bind:key="index">
-        <input 
-          :disabled="this.answerSubmitted" 
-          type="radio" name="options" 
-          :value="answer" 
-          v-model="this.chosen_answer">
-          
-        <label v-html="answer"></label><br>
-      </template>
-
-      <button v-if="!this.answerSubmitted" @click="this.submitAnswer()" class="send" type="button">Send</button>
-
-    </template>
-    
-
-    
-    <section class="result" v-if="this.answerSubmitted">
-      <template v-if="this.chosen_answer == this.correctAnswer">
-        <h4>&#9989; Congratulations, the answer "{{this.correctAnswer}}" is correct.</h4>
-      </template>
-      <template v-else>
-        <h4>&#10060;  I'm sorry, you picked the wrong answer. The correct is "{{this.correctAnswer}}".</h4>
-      </template>
-      <button @click="this.getNewQuestion()" class="send" type="button">Next question</button>
-    </section>
-
+  <div id="nav">
+    <router-link to="/">Home</router-link> -
+    <router-link to="/basket">Shopping Bag ({{this.productsInBag.length}})</router-link> 
+  </div>
+  <router-view/>
 </template>
 
 <script>
 
-export default {
-  name: 'App',
-  data() {
-    return {
-      chosen_answer: undefined,
-      question: undefined,
-      incorrectAnswers: [],
-      correctAnswer: '',
-      win_count: 0,
-      lose_count: 0,
-      answerSubmitted: false
-    }
-  },
-  computed: {
-      answers() {
-        var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
-        answers.splice(Math.round(Math.random() * answers.length), 0, this.correctAnswer);
-        return answers;
-      }
-  },
-  methods: {
-    submitAnswer() {
-      if (!this.chosen_answer) {
-        alert('Pick one of the options');
-      } else {
-        this.answerSubmitted = true;
-        if (this.chosen_answer == this.correctAnswer) {
-          this.win_count++;
-        } else {
-          this.lose_count++
-        }
-      }
-    },
-    getNewQuestion() {
-      this.chosen_answer = undefined;
-      this.answerSubmitted = false;
-      this. question = undefined;
+  import { mapState } from 'vuex'
+  export default {
 
-      this.axios
-      .get('https://opentdb.com/api.php?amount=1&category=30')
-      .then(response => (
-        this.question = response.data.results[0].question,
-        this.incorrectAnswers = response.data.results[0].incorrect_answers,
-        this.correctAnswer = response.data.results[0].correct_answer
-      ))
-    }
-  },
-  created() {
-  
-    this.getNewQuestion();
+    created() {
+      this.$store.dispatch('loadProducts');
+      this.$store.dispatch('loadBag');
+    },
+    computed: mapState([
+      'productsInBag' 
+    ]),
 
   }
-}
-
 </script>
+
 
 <style lang="scss">
 #app {
@@ -102,39 +30,29 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin: 60px auto;
-  max-width: 960px;
-
+  max-width: 1280px;
+  margin: 80px auto;
 }
 
-h1 {
-  margin-top: 40px;
-}
+#nav {
+  padding: 16px 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  width: 100%;
+  text-align: center;
+  background-color: rgb(37, 37, 37);
+  color: white;
 
-input[type='radio']{
-  margin: 12px 4px;
-}
 
-button.send {
-  margin-top: 12px;
-  height: 40px;
-  min-width: 120px;
-  padding: 0 16px;
-  color: #fff;
-  background-color: #1867c0;
-  border: 1px solid #1867c0;
-  cursor: pointer;
-}
+  a {
+    color: white;
+    text-decoration: none;
 
-section.score {
-  border-bottom: 1px solid black;
-  padding: 24px;
-  font-size: 18px;
-
-  span {
-    padding: 8px;
-    font-weight: bold;
-    border: 1px solid black;
+    &.router-link-exact-active {
+      color: #007bff;
+    }
   }
 }
 </style>
