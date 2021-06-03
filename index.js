4ce411b4daaf8df6db20132c140e3724d47abc31
Narul <naruln9494@gmail.com>
2021-06-03 17:25:11 +0600

    user

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
+const User = {
+    data() {
+        return {
+            user: {},
+            loading: true
+        };
+    },
+    async beforeRouteEnter(to, from, next) {
+        const user = await Promise.resolve({ id: to.params.id });
+        next(vm => vm.setUser(user));
+    },
+    async beforeRouteUpdate(to, from, next) {
+        const user = await Promise.resolve({ id: to.params.id });
+        this.setUser(user);
+        next();
+    },
+    methods: {
+        setUser(user) {
+            this.loading = true;
+            this.user = user;
+            this.loading = false;
+        }
+    },
+    template: `
+    <div>
+      <div v-if='!loading'>user: {{user.id}}</div>
+      <div v-if='loading'>Loading</div>
+    </div>
+  `
+};
+
+const routes = [
+    {
+        path: '/:id',
+        component: User
+    }
+];
+
+const router = new VueRouter({
+    routes
+});
+
 new Vue({
     el: '#app',
-    data: {
-        firstName: 'Joe',
-        lastName: 'Smith'
-    },
-    computed: {
-        name: {
-            get() {
-                return `${this.firstName} ${this.lastName}`;
-            },
-            set(newValue) {
-                const [firstName, lastName] = newValue.split(' ');
-                this.firstName = (firstName || '').trim();
-                this.lastName = (lastName || '').trim();
-            }
-        }
-    }
+    router
 });
