9d9a63133ec2e540a0e09b09cc7a69b3da4924e8
Narul <naruln9494@gmail.com>
2021-06-03 17:03:52 +0600

    computed

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-new Vue({
+const vm = new Vue({
     el: '#app',
-    data: { states: ['foo', 'bar', 'baz'], state: '', index: 0 },
-    methods: {
-        rotate() {
-            this.index = (this.index + 1) % this.states.length;
-            this.state = this.states[this.index];
+    data: {
+        message: 'foo'
+    },
+    computed: {
+        spelledMessage() {
+            return this.message.split('').join('-');
         }
     }
-});
+}); console.log(vm.spelledMessage);
